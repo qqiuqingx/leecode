c91cd9a2b645319440461e516087eddf0da6e739
qiuqingx <qqiuqingx@163.com>
2021-12-14 20:38:33 +0800

    最大子数组和

---
+++ src/com/company/maxSubArray/Solution.java
@@ -0,0 +1,25 @@
+package com.company.maxSubArray;
+
+/**
+ *  最大子数组和
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 子数组 是数组中的一个连续部分
+ */
+public class Solution {
+    public static int maxSubArray(int[] nums) {
+
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2,1}));
+    }
+
+
+}
